19ab7fc830ebd50c7ec364db91dd17652b43e12a
chira <chira2chira2chira2chira@gmail.com>
2024-02-28 22:16:31 +0900

    prefetchが動くためページを分ける

--- src/pages/discord/connect/index.tsx
+++ src/pages/discord/connect/index.tsx
@@ -1,73 +1,20 @@
-import { GetServerSideProps, NextPage } from "next";
+import { NextPage } from "next";
 import { useRouter } from "next/router";
-import { getIronSession } from "iron-session";
-import { useContext, useEffect } from "react";
-import { ConnectContext } from "../../../providers/ConnectProvider";
-import { SessionData, sessionOptions } from "../../../utils/session";
+import { useEffect } from "react";
 import CommonMeta from "../../../components/CommonMeta";
 
-type ConnectProps = {
-  success: boolean;
-  message: string;
-};
-
-const Connect: NextPage<ConnectProps> = (props) => {
-  const { push, asPath } = useRouter();
-  const { connect } = useContext(ConnectContext);
+const Connect: NextPage = () => {
+  const { query, replace, asPath } = useRouter();
 
   useEffect(() => {
-    if (props.success) connect();
-    push(`/?connectMessage=${encodeURI(props.message)}`, "/");
-  }, [push, asPath, props, connect]);
+    replace(`/discord/getid?key=${query.key}`);
+  }, [replace, asPath, query]);
 
   return (
     <>
-      <CommonMeta title="ワルトボタン" cardType="summary" />
+      <CommonMeta title="ワルトボタンと接続する" cardType="summary" />
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps<ConnectProps> = async (
-  context
-) => {
-  const key = context.query.key;
-
-  try {
-    const res = await fetch(`${process.env.BOT_ENDPOINT}/connect`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": process.env.BOT_API_KEY || "",
-      },
-      body: JSON.stringify({ key }),
-    });
-
-    const { id, message } = await res.json();
-    if (res.ok) {
-      const session = await getIronSession<SessionData>(
-        context.req,
-        context.res,
-        sessionOptions
-      );
-      session.channelId = id;
-      await session.save();
-    }
-
-    return {
-      props: {
-        success: res.ok,
-        message,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      props: {
-        success: false,
-        message: "Botとの通信に失敗しました。",
-      },
-    };
-  }
 };
 
 export default Connect;
