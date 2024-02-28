a5315fe412c5e63a9dfa79aa8123b40c907f1b0b
chira <chira2chira2chira2chira@gmail.com>
2024-02-29 00:35:09 +0900

    Onetimeリンクをやめたのでページ分割を戻す

--- src/pages/discord/connect/index.tsx
+++ src/pages/discord/connect/index.tsx
@@ -1,21 +1,31 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { getIronSession } from "iron-session";
+import { useContext, useEffect } from "react";
+import { ConnectContext } from "../../../providers/ConnectProvider";
+import { SessionData, sessionOptions } from "../../../utils/session";
 import CommonMeta from "../../../components/CommonMeta";
 import { css } from "@emotion/react";
 
-const Connect: NextPage = () => {
-  const { query, replace, asPath } = useRouter();
+type ConnectProps = {
+  success: boolean;
+  message: string;
+};
+
+const Connect: NextPage<ConnectProps> = (props) => {
+  const { push, asPath } = useRouter();
+  const { connect } = useContext(ConnectContext);
 
   useEffect(() => {
-    replace(`/discord/getid?key=${query.key}`);
-  }, [replace, asPath, query]);
+    if (props.success) connect();
+    push(`/?connectMessage=${encodeURI(props.message)}`, "/");
+  }, [push, asPath, props, connect]);
 
   return (
     <>
       <CommonMeta
         title="ワルトボタンと接続する"
-        description="※このリンクは1回のみ有効"
+        description="※このリンクは5分間有効"
         cardType="summary"
       />
 
@@ -42,6 +52,49 @@ const Connect: NextPage = () => {
       </div>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<ConnectProps> = async (
+  context
+) => {
+  const key = context.query.key;
+
+  try {
+    const res = await fetch(`${process.env.BOT_ENDPOINT}/connect`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.BOT_API_KEY || "",
+      },
+      body: JSON.stringify({ key }),
+    });
+
+    const { id, message } = await res.json();
+    if (res.ok) {
+      const session = await getIronSession<SessionData>(
+        context.req,
+        context.res,
+        sessionOptions
+      );
+      session.channelId = id;
+      await session.save();
+    }
+
+    return {
+      props: {
+        success: res.ok,
+        message,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        success: false,
+        message: "Botとの通信に失敗しました。",
+      },
+    };
+  }
 };
 
 export default Connect;
