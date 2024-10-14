6a8272d9b2aff87a78a0b074ffbab00c6e8f94c3
chira <chira2chira2chira2chira@gmail.com>
2024-10-15 01:51:42 +0900

    表示崩れ対応
    シェアボタン読み込み時にサイト全体が縮小してしまう問題に対応
    シェアボタン読み込み前と後で高さを合わせる

--- src/components/TweetButton/index.tsx
+++ src/components/TweetButton/index.tsx
@@ -6,7 +6,7 @@ type TweetButtonProps = {
 
 const TweetButton: React.VFC<TweetButtonProps> = (props) => {
   return (
-    <div style={{ colorScheme: "auto" }}>
+    <div style={{ colorScheme: "auto", height: "24px" }}>
       <a
         href="https://twitter.com/share?ref_src=twsrc%5Etfw"
         className="twitter-share-button"
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="theme-color" content="#182026" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
