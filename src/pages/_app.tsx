2f64d8598678331aad396247a15e86662eca38e8
chira <chira2chira2chira2chira@gmail.com>
2024-10-19 02:07:20 +0900

    Viewportは_app.tsxで宣言するのが推奨されている

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import "normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import { FocusStyleManager } from "@blueprintjs/core";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
@@ -14,7 +16,6 @@ import ErrorBoundary from "../components/ErrorBoundary";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 import usePageView from "../hooks/usePageView";
 import useDarkMode from "../hooks/useDarkMode";
-import { useRouter } from "next/router";
 
 FocusStyleManager.onlyShowFocusOnTabs();
 
@@ -27,28 +28,33 @@ function MyApp({ Component, pageProps }: AppProps) {
   useDarkMode();
 
   return (
-    <ErrorBoundary>
-      <script
-        // レンダリング前にダークモード用のクラスをセットする
-        dangerouslySetInnerHTML={{
-          __html: `document.addEventListener("DOMContentLoaded", () => {
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <ErrorBoundary>
+        <script
+          // レンダリング前にダークモード用のクラスをセットする
+          dangerouslySetInnerHTML={{
+            __html: `document.addEventListener("DOMContentLoaded", () => {
             if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
               document.body.className = "bp5-dark";
-            } else {
-              document.body.className = "bp5-body";
+              } else {
+                document.body.className = "bp5-body";
             }
-          });`,
-        }}
-      />
-      <GoogleAnalytics />
-      <VolumeProvider>
-        <FavoriteProvider>
-          <ConnectProvider>
-            <Component {...pageProps} />
-          </ConnectProvider>
-        </FavoriteProvider>
-      </VolumeProvider>
-    </ErrorBoundary>
+            });`,
+          }}
+        />
+        <GoogleAnalytics />
+        <VolumeProvider>
+          <FavoriteProvider>
+            <ConnectProvider>
+              <Component {...pageProps} />
+            </ConnectProvider>
+          </FavoriteProvider>
+        </VolumeProvider>
+      </ErrorBoundary>
+    </>
   );
 }
 export default MyApp;
